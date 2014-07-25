368d92703ad1611bf1a1145e77ad0708c0a41fdf
Jake Rosoman <jkroso@gmail.com>
2014-07-26 09:41:04 +1200

    test invalid file ref

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,27 @@
 import Require.require
+using Base.Test
 
 a = require("a")
 
-@assert isa(a, Module)
-@assert a.a == 1
-@assert a.b == 2
-@assert a.c == 3
+@test isa(a, Module)
+@test a.a == 1
+@test a.b == 2
+@test a.c == 3
 
 b = require("b")
 
-@assert isa(b, Module)
-@assert b.a == "hi"
+@test isa(b, Module)
+@test b.a == "hi"
 
 ab = require("a/b")
 
-@assert isa(ab, Module)
-@assert ab.a == "secret"
+@test isa(ab, Module)
+@test ab.a == "secret"
 
-@assert is(require("a/b"), require("a/b"))
+@test is(require("a/b"), require("a/b"))
 
-@assert is(require("a/c").a, require("a/b"))
+@test is(require("a/c").a, require("a/b"))
 
-@assert is(require("c").a, require("b"))
+@test is(require("c").a, require("b"))
+
+@test_throws ErrorException require("casper")
